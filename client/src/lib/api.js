545fcb1852448db5c9e89a402f9b9b883963d617
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-07-24 22:36:25 +0100

    feat(client): add layout with nav and side bar

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -10,9 +10,19 @@ export const login = async (loginData) => {
   return response.data.data;
 };
 
-export const getAuthUser = async () => {
-  const response = await axiosInstance.get("/auth/me");
+export const logout = async () => {
+  const response = await axiosInstance.post("/auth/logout");
   return response.data.data;
+};
+
+export const getAuthUser = async () => {
+  try {
+    const response = await axiosInstance.get("/auth/me");
+    return response.data.data;
+  } catch (error) {
+    console.log("Error in getAuthUser:", error);
+    return null;
+  }
 };
 
 export const completeOnboarding = async (userData) => {
